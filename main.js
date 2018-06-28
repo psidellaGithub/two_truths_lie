6ba29ad35cfc5829a2c4ed9506a9897653db8271
priynka <psidella@gmail.com>
2018-06-27 19:40:55 -0500

    bodercolor

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+            $('.box').hover(function () {
+              // $('.box1').css("background-color", "white");
+              // $('.box2').css("background-color", "white");
+                $('.w').css("color", "black");
+                $('.box7').css("background-color", "green");
+                $('.c').css("color", "green");
+
+            })
+
+
+            $('.box1').click(function () {
+               $('.box1').css("border-color", "green");
+
+            })
+            $('.box2').click(function () {
+               $('.box2').css("border-color", "green");
+
+            })
+            $('.box3').click(function () {
+               $('.box3').css("border-color", "red");
+
+            })
+  })
+
+            // $('.box2').hover(function () {
+            //   $('.box7').css("background-color", "white");
+            //   $('.box6').css("background-color", "white");
+            //   $('.c').css("color", "black");
+            //     $('.box5').css("background-color", "red");
+            //     $('.w').css("color", "red");
+            //
+            // })
+            // $('.box3').hover(function () {
+            //   $('.box5').css("background-color", "white");
+            //   $('.box7').css("background-color", "white");
+            //     $('.c').css("color", "black");
+            //     $('.box6').css("background-color", "red");
+            //     $('.w').css("color", "red");
+            //
+            // })
